f4971d08a9bc06b4a420431faa59dcf254e9df58
Parag0712 <paragvadgama123@gmail.com>
2024-02-13 15:44:48 +0530

    Vite app created

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import { ThemeProvider } from '@emotion/react'
+import { theme } from './theme.ts'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>
+)
---
+++ src/theme.ts
@@ -0,0 +1,18 @@
+import { createTheme } from "@mui/material";
+
+
+export const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#9999ff',
+        },
+        secondary: {
+            main: '#6b7db3',
+        },
+        background: {
+            default: '#e6ecff',
+            paper: '#ccd9ff',
+        },
+    },
+})
+
