82d3050a58eced2783265c4f647f1f258c1cde12
Parag0712 <paragvadgama123@gmail.com>
2024-02-14 20:26:06 +0530

    Quiz ui done and redux setup done

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -1,6 +1,61 @@
+import { Button, Container, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, Typography } from "@mui/material";
+import { useState } from "react"
+import { useNavigate } from "react-router-dom";
+
 function Quiz() {
+    const [result, setResult] = useState<string[]>([]);
+    const [count, setCount] = useState<number>(0);
+    const [answer, setAnswer] = useState<string>("");
+
+    const navigate = useNavigate();
+    // 
+    const nextHandler = (): void => {
+        setResult((prev) => ([...prev, answer]))
+        setCount((prev) => prev + 1);
+        setAnswer("")
+    }
     return (
-        <div>Quiz</div>
+        <Container
+            maxWidth="sm"
+            sx={{
+                padding: "1rem",
+            }}
+        >
+            <Typography m={"2rem 0"}>Quiz</Typography>
+
+            <Typography variant={"h3"}>
+                {count + 1} - {"Random"}
+            </Typography>
+
+            <FormControl>
+                <FormLabel
+                    sx={{
+                        mt: "2rem",
+                        mb: "1rem",
+                    }}
+                >
+                    Meaning
+                </FormLabel>
+                <RadioGroup value={answer} onChange={(e) => setAnswer(e.target.value)}>
+                        <FormControlLabel
+                            value={"Lol"}
+                            control={<Radio />}
+                            label={"option"}
+                        />
+                </RadioGroup>
+            </FormControl>
+
+            <Button
+                sx={{
+                    margin: "3rem 0",
+                }}
+                variant="contained"
+                fullWidth
+                disabled={answer === ""}
+            >
+                {count === 7?"Submit":"Next"}
+            </Button>
+        </Container>
     )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import App from './App.tsx'
 import { ThemeProvider } from '@emotion/react'
 import { theme } from './theme.ts'
 import React from 'react'
+import { Provider } from 'react-redux'
+import { store } from './Redux/store.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </ThemeProvider>
   </React.StrictMode>
 )
